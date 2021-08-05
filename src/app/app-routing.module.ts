72b67a910b509018a9159bd4e3017dcad98e532a
frankmckechnie <frankmckechnie@gmail.com>
2021-08-05 22:43:17 +0100

    feat(workout-page): added the bare to get the provider setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,14 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./pages/tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'account',
+    loadChildren: () => import('./pages/account/account.module').then( m => m.AccountPageModule)
+  },
+  {
+    path: 'player',
+    loadChildren: () => import('./pages/player/player.module').then( m => m.PlayerPageModule)
   }
 ];
 @NgModule({
--- src/app/services/workout/workout.service.ts
+++ src/app/services/workout/workout.service.ts
@@ -9,11 +9,13 @@ export class WorkoutService {
   private data: Workout[] = [
     {
       uid: 'saddasd-asdasd-asd',
-      name: 'Week 1'
+      name: 'Week 1',
+      description: 'Test description'
     },
     {
       uid: 'saddasd-ascdcsdc-asd',
-      name: 'Week 2'
+      name: 'Week 2',
+      description: 'Test description'
     }
   ];
 
